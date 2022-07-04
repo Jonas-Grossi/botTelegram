7464be16416d2b6ae89b2408769a67ea4a2e0153
Jonas-Grossi <jonasgrossicc@gmail.com>
2022-07-04 16:48:49 -0300

    bot v.1.0

---
+++ bot/bot.js
@@ -0,0 +1,23 @@
+const Telegraf = require('telegraf')
+const env = require('../.env')
+const bot =new Telegraf(env.token)
+
+bot.start(ctx =>{
+const from = ctx.update.message.from
+console.log(ctx.update.message.from)
+ctx.reply(`Seja bem vindo, ${from.first_name}!`)
+
+})
+
+bot.on('text',async(ctx,next)=>{
+
+await ctx.reply('Mid 1')
+next()
+
+})
+bot.on('text', async ctx =>{
+
+   await ctx.reply('Mid 2')
+    
+    })
+bot.startPolling()
